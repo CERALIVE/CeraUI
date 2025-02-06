acbc723b6a68cfa453e1e5ebca11097ed1d14bdf
Paul Golmann <mail@pje-web.de>
2025-02-06 22:20:38 +0100

    hotfix: disable the belaUI socket for now

--- deploy-to-local.sh
+++ deploy-to-local.sh
@@ -20,7 +20,15 @@ echo "Copying assets"
 bun run copy
 
 echo "Deploying to $RSYNC_TARGET"
-rsync -rltvz --delete --chown=root:root --exclude auth_tokens.json --exclude config.json --exclude dns_cache.json --exclude gsm_operator_cache.json --exclude setup.json "${DIST_PATH}/" $RSYNC_TARGET
+rsync -rltvz --delete --chown=root:root \
+  --exclude auth_tokens.json \
+  --exclude config.json \
+  --exclude dns_cache.json \
+  --exclude gsm_operator_cache.json \
+  --exclude relays_cache.json \
+  --exclude revision \
+  --exclude setup.json \
+  "${DIST_PATH}/" $RSYNC_TARGET
 
 echo "Installing and restarting service"
 # shellcheck disable=SC2029
@@ -30,23 +38,25 @@ ssh "$SSH_TARGET" "cd $BELAUI_PATH; ./install_service.sh"
 echo "Killing belaUI"
 ssh "$SSH_TARGET" "pkill belaUI || true"
 
-echo "Stop and disable service/socket"
-ssh "$SSH_TARGET" "systemctl stop belaUI.socket || true"
-ssh "$SSH_TARGET" "systemctl stop belaUI.service || true"
+echo "Stop belaUI"
+ssh "$SSH_TARGET" "systemctl stop belaUI || true"
+
+echo "Disable service/socket"
 ssh "$SSH_TARGET" "systemctl disable belaUI.socket || true"
 ssh "$SSH_TARGET" "systemctl disable belaUI.service || true"
 
 echo "Update service/socket"
 ssh "$SSH_TARGET" "systemctl daemon-reload"
-ssh "$SSH_TARGET" "systemctl enable belaUI.socket"
+
+echo "Enable service/socket"
 ssh "$SSH_TARGET" "systemctl enable belaUI.service"
+#ssh "$SSH_TARGET" "systemctl enable belaUI.socket"
 
 echo "Starting belaUI"
-ssh "$SSH_TARGET" "systemctl start belaUI.socket"
 ssh "$SSH_TARGET" "systemctl start belaUI.service"
 
 echo "Watch belaUI"
-ssh "$SSH_TARGET" "journalctl -u belaUI.service -f"
+ssh "$SSH_TARGET" "journalctl -u belaUI -n 200 -f"
 
 #echo "Starting belaUI"
 #ssh "$SSH_TARGET" "cd $BELAUI_PATH; ./belaUI"
