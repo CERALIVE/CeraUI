48e8cda70301bdf91cb925099b7bf1a1b0cffc7d
Paul Golmann <mail@pje-web.de>
2025-02-15 00:11:10 +0100

    chore: cleanup install/deploy/override scripts

--- deploy-to-local.sh
+++ deploy-to-local.sh
@@ -10,15 +10,6 @@ RSYNC_TARGET="${SSH_TARGET}:${BELAUI_PATH}"
 # stop on error
 set -e
 
-echo "Installing dependencies"
-bun install
-
-echo "Building"
-bun run build:linux-arm64
-
-echo "Copying assets"
-bun run copy
-
 echo "Deploying to $RSYNC_TARGET"
 rsync -rltvz --delete --chown=root:root \
   --exclude auth_tokens.json \
@@ -30,35 +21,5 @@ rsync -rltvz --delete --chown=root:root \
   --exclude setup.json \
   "${DIST_PATH}/" $RSYNC_TARGET
 
-echo "Installing and restarting service"
 # shellcheck disable=SC2029
-ssh "$SSH_TARGET" "cd $BELAUI_PATH; ./install_service.sh"
-
-echo "Installing Moblink Relay"
-# shellcheck disable=SC2029
-ssh "$SSH_TARGET" "cd $BELAUI_PATH; ./install-moblink-rust-relay.sh"
-
-# Kill any running belaUI
-echo "Killing belaUI"
-ssh "$SSH_TARGET" "pkill belaUI || true"
-
-echo "Stop belaUI"
-ssh "$SSH_TARGET" "systemctl stop belaUI.socket || true"
-ssh "$SSH_TARGET" "systemctl stop belaUI.service || true"
-
-echo "Disable service/socket"
-ssh "$SSH_TARGET" "systemctl disable belaUI.socket || true"
-ssh "$SSH_TARGET" "systemctl disable belaUI.service || true"
-
-echo "Update service/socket"
-ssh "$SSH_TARGET" "systemctl daemon-reload"
-
-echo "Enable service/socket"
-ssh "$SSH_TARGET" "systemctl enable belaUI.service"
-#ssh "$SSH_TARGET" "systemctl enable belaUI.socket"
-
-echo "Starting belaUI"
-ssh "$SSH_TARGET" "systemctl start belaUI.service"
-
-echo "Watch belaUI"
-ssh "$SSH_TARGET" "journalctl -u belaUI -n 200 -f"
+ssh "$SSH_TARGET" "cd $BELAUI_PATH; sudo ./override-belaui.sh"
--- dist/install_service.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sed "s#WorkingDirectory=.*#WorkingDirectory=$(pwd)#g" belaUI.service > /etc/systemd/system/belaUI.service &&
-cp belaUI.socket /etc/systemd/system/
-systemctl daemon-reload &&
-systemctl restart belaUI &&
-systemctl enable belaUI.socket
-systemctl enable belaUI.service
-cp *.rules /etc/udev/rules.d/
