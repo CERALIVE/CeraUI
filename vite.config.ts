ad8ef466c1501d6213f7aaf7ebfd222c5511d03c
Andres Cera <andres.cera@hotmail.com>
2025-06-22 23:48:57 -0500

    chore: unnecesary full reload was removed & component inspector enabled

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,14 @@ import tailwindcss from '@tailwindcss/vite';
 import * as path from 'path';
 import { defineConfig } from 'vite';
 
-const fullReloadAlways = {
-  name: 'full-reload',
-  //eslint-disable-next-line @typescript-eslint/no-explicit-any
-  handleHotUpdate({ server }: { server: any }) {
-    server.ws.send({ type: 'full-reload' });
-    return [];
-  },
-};
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
     svelte({
       compilerOptions: { hmr: true },
+      inspector: { showToggleButton: 'always', toggleButtonPos: 'bottom-right' },
     }),
-    fullReloadAlways,
   ],
   publicDir: './src/assets',
   resolve: {
