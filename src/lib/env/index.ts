75174e391306265e5516df0c5536aa3adb15dd8a
Andres Cera <andres.cera@hotmail.com>
2025-07-14 09:08:07 -0500

    Refactor: Simplify environment variable handling
    - Added reusable `getEnvVariable` function to handle `SOCKET_ENDPOINT` and `SOCKET_PORT` based on the environment.

--- src/lib/env/index.ts
+++ src/lib/env/index.ts
@@ -1,4 +1,22 @@
-export const ENV_VARIABLES = {
-  SOCKET_ENDPOINT: import.meta.env.VITE_SOCKET_ENDPOINT || `ws://${window.location.hostname}`,
-  SOCKET_PORT: import.meta.env.VITE_SOCKET_PORT || '80',
+interface EnvVariables {
+  SOCKET_ENDPOINT: string;
+  SOCKET_PORT: string;
+}
+
+export const ENV_VARIABLES: EnvVariables = {
+  SOCKET_ENDPOINT: getEnvVariable('SOCKET_ENDPOINT', `ws://${window.location.hostname}`),
+  SOCKET_PORT: getEnvVariable('SOCKET_PORT', '80'),
 };
+
+function getEnvVariable(variable: 'SOCKET_ENDPOINT' | 'SOCKET_PORT', defaultValue: string): string {
+  // Retrieve the value from the environment variables (prefixed with 'VITE_' in Vite)
+  const envValue = import.meta.env[`VITE_${variable}`];
+
+  // If in development mode and the environment variable is available, use it
+  if (import.meta.env.MODE === 'development' && envValue) {
+    return envValue;
+  }
+
+  // In production or when no env value is found, fallback to the provided default value
+  return defaultValue;
+}
